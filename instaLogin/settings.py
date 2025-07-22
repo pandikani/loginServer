29476e4e127376608f47aebde41352e7d67bc6be
pandikani <pandikani2101@gmail.com>
2025-07-22 08:58:52 +0530

    @cores@

--- instaLogin/settings.py
+++ instaLogin/settings.py
@@ -52,7 +52,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://molshey.vercel.app",
+    "http://localhost:3000",
+]
+
 ROOT_URLCONF = 'instaLogin.urls'
 
 TEMPLATES = [
