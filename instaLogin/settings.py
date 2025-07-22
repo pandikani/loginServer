fb984d02dc85ef91447450387811737fa4802f1f
pandikani <pandikani2101@gmail.com>
2025-07-22 08:40:47 +0530

    DB added

--- instaLogin/settings.py
+++ instaLogin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s3h(q@ux3fx*^-__%m(us+%e9h)4b#=i3ioqnxd-e_g3sluin$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
