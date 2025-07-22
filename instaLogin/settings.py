e3f532cd255f99f387f637b01fd5fed45a80aceb
pandikani <pandikani2101@gmail.com>
2025-07-22 09:04:41 +0530

    @cores@

--- instaLogin/settings.py
+++ instaLogin/settings.py
@@ -44,17 +44,21 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 CORS_ALLOWED_ORIGINS = [
     "https://molshey.vercel.app",
     "http://localhost:3000",
+    "http://localhost:8000",
 ]
 
 ROOT_URLCONF = 'instaLogin.urls'
