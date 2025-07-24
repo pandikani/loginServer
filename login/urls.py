faac2078af04fa555793c2752c655c9ab1ff37e7
pandikani <pandikani2101@gmail.com>
2025-07-24 18:44:54 +0530

    cha

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,9 @@
 # accounts/urls.py
 from django.urls import path
 from .views import login_view
+from django.contrib import admin
 
 urlpatterns = [
     path('login_next/', login_view),
+    path('admin/', admin.site.urls),
 ]
